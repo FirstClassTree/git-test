d8ec79f500a91de9f35faa662977fbec062c9bec
FirstClassTree <talweissexclusive@gmail.com>
2023-11-29 17:48:40 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const mike = 'mike'
+
+const sayHi = (name) => {
+    console.log(`hello ${name}`)
+    return 50;
+
+}
+
+sayHi('susan')
+sayHi(mike)
+console.log(sayHi(mike));
+
+const kid = [5]
+kid[0]= 1
+kid[2] =3
+
+console.log(kid[0])
+console.log(kid[2])
+kid[1]
